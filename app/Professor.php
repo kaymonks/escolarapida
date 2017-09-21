46c07759265645dc02a3e6263c9e228999f208e4
Kaymon Storino <kaymon.storino@gmail.com>
2017-09-21 19:03:43 -0300

    adiciona model Professor

--- app/Professor.php
+++ app/Professor.php
@@ -6,12 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Professor extends Model
 {
-    protected $fillable = [
-        'nome', 'telefone', 'login', 'senha',
-    ];
+    protected $table = 'professores';
+    protected $primaryKey = 'id';
+    protected $fillable = ['nome', 'telefone'];
 
-    public function Turmas()
+    public function turmas()
     {
-        return $this->belongsToMany('App\Turma')->withTimestamps();
+        return $this->belongsToMany('App\Turma', 'turma_professores', 'professor_id', 'turma_id');
     }
 }
