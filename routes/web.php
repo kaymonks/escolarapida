e5d730fd2451c97ef6efaf980b4da66095603f83
Kaymon Storino <kaymon.storino@gmail.com>
2017-09-21 17:11:21 -0300

    adiciona rotas de login e pais

--- routes/web.php
+++ routes/web.php
@@ -15,23 +15,39 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/escolas', ['as'=>'escolas', 'uses'=>'escolaController@index']);
-Route::get('/escola/adicionar', ['as'=>'escola.adicionar', 'uses'=>'escolaController@adicionar']);
-Route::post('/escola/salvar', ['as'=>'escola.salvar', 'uses'=>'escolaController@salvar']);
-Route::get('/escola/editar/{id}', ['as'=>'escola.editar', 'uses'=>'escolaController@editar']);
-Route::put('/escola/atualizar/{id}', ['as'=>'escola.atualizar', 'uses'=>'escolaController@atualizar']);
-Route::get('/escola/deletar/{id}', ['as'=>'escola.deletar', 'uses'=>'escolaController@deletar']);
+Route::get('/login', ['as'=>'login', 'uses'=>'LoginController@index']);
+Route::post('/login/entrar', ['as'=>'login.entrar', 'uses'=>'LoginController@entrar']);
+Route::get('/login/sair', ['as'=>'login.sair', 'uses'=>'LoginController@sair']);
 
-Route::get('/professores', ['as'=>'professores', 'uses'=>'ProfessorController@index']);
-Route::get('/professor/adicionar', ['as'=>'professor.adicionar', 'uses'=>'ProfessorController@adicionar']);
-Route::post('/professor/salvar', ['as'=>'professor.salvar', 'uses'=>'professorController@salvar']);
-Route::get('/professor/editar/{id}', ['as'=>'professor.editar', 'uses'=>'professorController@editar']);
-Route::put('/professor/atualizar/{id}', ['as'=>'professor.atualizar', 'uses'=>'professorController@atualizar']);
-Route::get('/professor/deletar/{id}', ['as'=>'professor.deletar', 'uses'=>'professorController@deletar']);
 
-Route::get('/turmas', ['as'=>'turmas', 'uses'=>'TurmaController@index']);
-Route::get('/turma/adicionar', ['as'=>'turma.adicionar', 'uses'=>'TurmaController@adicionar']);
-Route::post('/turma/salvar', ['as'=>'turma.salvar', 'uses'=>'TurmaController@salvar']);
-Route::get('/turma/editar/{id}', ['as'=>'turma.editar', 'uses'=>'TurmaController@editar']);
-Route::put('/turma/atualizar/{id}', ['as'=>'turma.atualizar', 'uses'=>'TurmaController@atualizar']);
-Route::get('/turma/deletar/{id}', ['as'=>'turma.deletar', 'uses'=>'TurmaController@deletar']);
+Route::group(['middleware'=>'auth'], function (){
+
+    Route::get('/escolas', ['as'=>'escolas', 'uses'=>'escolaController@index']);
+    Route::get('/escola/adicionar', ['as'=>'escola.adicionar', 'uses'=>'escolaController@adicionar']);
+    Route::post('/escola/salvar', ['as'=>'escola.salvar', 'uses'=>'escolaController@salvar']);
+    Route::get('/escola/editar/{id}', ['as'=>'escola.editar', 'uses'=>'escolaController@editar']);
+    Route::put('/escola/atualizar/{id}', ['as'=>'escola.atualizar', 'uses'=>'escolaController@atualizar']);
+    Route::get('/escola/deletar/{id}', ['as'=>'escola.deletar', 'uses'=>'escolaController@deletar']);
+
+    Route::get('/professores', ['as'=>'professores', 'uses'=>'ProfessorController@index']);
+    Route::get('/professor/adicionar', ['as'=>'professor.adicionar', 'uses'=>'ProfessorController@adicionar']);
+    Route::post('/professor/salvar', ['as'=>'professor.salvar', 'uses'=>'professorController@salvar']);
+    Route::get('/professor/editar/{id}', ['as'=>'professor.editar', 'uses'=>'professorController@editar']);
+    Route::put('/professor/atualizar/{id}', ['as'=>'professor.atualizar', 'uses'=>'professorController@atualizar']);
+    Route::get('/professor/deletar/{id}', ['as'=>'professor.deletar', 'uses'=>'professorController@deletar']);
+
+    Route::get('/turmas', ['as'=>'turmas', 'uses'=>'TurmaController@index']);
+    Route::get('/turma/adicionar', ['as'=>'turma.adicionar', 'uses'=>'TurmaController@adicionar']);
+    Route::post('/turma/salvar', ['as'=>'turma.salvar', 'uses'=>'TurmaController@salvar']);
+    Route::get('/turma/editar/{id}', ['as'=>'turma.editar', 'uses'=>'TurmaController@editar']);
+    Route::put('/turma/atualizar/{id}', ['as'=>'turma.atualizar', 'uses'=>'TurmaController@atualizar']);
+    Route::get('/turma/deletar/{id}', ['as'=>'turma.deletar', 'uses'=>'TurmaController@deletar']);
+
+    Route::get('/pais', ['as'=>'pais', 'uses'=>'PaiController@index']);
+    Route::get('/pai/adicionar', ['as'=>'pai.adicionar', 'uses'=>'PaiController@adicionar']);
+    Route::post('/pai/salvar', ['as'=>'pai.salvar', 'uses'=>'PaiController@salvar']);
+    Route::get('/pai/editar/{id}', ['as'=>'pai.editar', 'uses'=>'PaiController@editar']);
+    Route::put('/pai/atualizar/{id}', ['as'=>'pai.atualizar', 'uses'=>'PaiController@atualizar']);
+    Route::get('/pai/deletar/{id}', ['as'=>'pai.deletar', 'uses'=>'PaiController@deletar']);
+});
+
