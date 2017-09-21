66f3ecee55f12534b0017bf1a117a994b0509356
Kaymon Storino <kaymon.storino@gmail.com>
2017-09-21 18:40:58 -0300

    modifica footer, header e sidebar

--- resources/views/layout/_includes/sidebar.blade.php
+++ resources/views/layout/_includes/sidebar.blade.php
@@ -9,41 +9,35 @@
                 <img src="{{ asset("/bower_components/AdminLTE/dist/img/user2-160x160.jpg") }}" class="img-circle" alt="User Image">
             </div>
             <div class="pull-left info">
-                <p>Alexander Pierce</p>
+                <p> @if(Auth::guest())
+
+                    @else
+                        {{ Auth::user()->name }}
+                    @endif</p>
                 <!-- Status -->
-                <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
             </div>
         </div>
 
-        <!-- search form (Optional) -->
-        <form action="#" method="get" class="sidebar-form">
-            <div class="input-group">
-                <input type="text" name="q" class="form-control" placeholder="Search...">
-                <span class="input-group-btn">
-                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
-                </button>
-              </span>
-            </div>
-        </form>
-        <!-- /.search form -->
-
         <!-- Sidebar Menu -->
         <ul class="sidebar-menu">
-            <li class="header">HEADER</li>
+            <li class="header">MENU</li>
             <!-- Optionally, you can add icons to the links -->
-            <li class="active"><a href="#"><i class="fa fa-link"></i> <span>Link</span></a></li>
-            <li><a href="#"><i class="fa fa-link"></i> <span>Another Link</span></a></li>
-            <li class="treeview">
-                <a href="#"><i class="fa fa-link"></i> <span>Multilevel</span>
-                    <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-                </a>
-                <ul class="treeview-menu">
-                    <li><a href="#">Link in level 2</a></li>
-                    <li><a href="#">Link in level 2</a></li>
-                </ul>
-            </li>
+            <li><a href="#"><i class="fa fa-home"></i> <span>Home</span></a></li>
+            <li class="{{ str_contains(request()->url(), '/escola') ? 'active' : '' }}"><a href="{{ route('escolas') }}"><i class="fa fa-institution"></i> <span>Escolas</span></a></li>
+            <li class="{{ str_contains(request()->url(), '/professor') ? 'active' : '' }}"><a href="{{ route('professores') }}"><i class="fa fa-user"></i> <span>Professores</span></a></li>
+            <li class="{{ str_contains(request()->url(), '/turma') ? 'active' : '' }}"><a href="{{ route('turmas') }}"><i class="fa fa-graduation-cap"></i> <span>Turmas</span></a></li>
+            <li class="{{ str_contains(request()->url(), '/pai') ? 'active' : '' }}"><a href="{{ route('pais') }}"><i class="fa fa-user"></i> <span>Pais</span></a></li>
+            {{--<li class="treeview">--}}
+                {{--<a href="#"><i class="fa fa-link"></i> <span>Multilevel</span>--}}
+                    {{--<span class="pull-right-container">--}}
+              {{--<i class="fa fa-angle-left pull-right"></i>--}}
+            {{--</span>--}}
+                {{--</a>--}}
+                {{--<ul class="treeview-menu">--}}
+                    {{--<li><a href="#">Link in level 2</a></li>--}}
+                    {{--<li><a href="#">Link in level 2</a></li>--}}
+                {{--</ul>--}}
+            {{--</li>--}}
         </ul>
         <!-- /.sidebar-menu -->
     </section>
