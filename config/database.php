e2867fd2a0462926850c032f7cb2998dc8f91dd3
Kaymon Storino <kaymon.storino@gmail.com>
2017-09-22 00:52:00 -0300

    adiciona variaves de producao

--- config/database.php
+++ config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-05.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_d9088c2a2a13b67'),
+            'username' => env('DB_USERNAME', 'b2307c9d13b08e'),
+            'password' => env('DB_PASSWORD', '845b1c70'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
