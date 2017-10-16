acf6994e679f6a058ea485ef4f7781ab4326fb4b
Kaymon Storino <kaymon.storino@gmail.com>
2017-10-16 01:21:07 -0200

    módulo pai

--- app/Http/Controllers/PaiController.php
+++ app/Http/Controllers/PaiController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Telefone;
 use Illuminate\Http\Request;
 use App\Pai;
+use DateTime;
+
 
 class PaiController extends Controller
 {
@@ -21,6 +24,17 @@ class PaiController extends Controller
     public function salvar(Request $request)
     {
         $dados = $request->all();
+
+        $telefone['telefone'] = $dados['telefone'];
+        $telefone2 = Telefone::create($telefone);
+        $dados['telefone_id'] = $telefone2->id;
+        $novaData = DateTime::createFromFormat('d/m/Y', $dados['data_nascimento']);
+        if ( false===$novaData )
+        {
+            die('formato de data inválido');
+        }
+        $dados['data_nascimento'] = $novaData->format('Y-m-d');
+
         Pai::create($dados);
 
         return redirect()->route('pais');
@@ -29,12 +43,23 @@ class PaiController extends Controller
     public function editar($id)
     {
         $registro = Pai::find($id);
-        return view('pai.editar', compact('registro'));
+        $registro['data_nascimento'] = date( 'd/m/Y' , strtotime($registro->data_nascimento ) );
+        $telefone = Pai::find($id)->telefones;
+        return view('pai.editar', compact('registro', 'telefone'));
     }
 
     public function atualizar(Request $request, $id)
     {
         $dados = $request->all();
+        $telefone['telefone'] = $dados['telefone'];
+        $telefone2 = Telefone::create($telefone);
+        $dados['telefone_id'] = $telefone2->id;
+        $novaData = DateTime::createFromFormat('d/m/Y', $dados['data_nascimento']);
+        if ( false===$novaData )
+        {
+            die('formato de data inválido');
+        }
+        $dados['data_nascimento'] = $novaData->format('Y-m-d');
         Pai::find($id)->update($dados);
         return redirect()->route('pais');
     }
