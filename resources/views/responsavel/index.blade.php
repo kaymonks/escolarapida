50ebce47fd45367307a478c90b60eb30cc1f80cc
Kaymon Storino <kaymon.storino@gmail.com>
2017-12-12 23:23:37 -0200

    altera de pais para responsáveis

--- resources/views/responsavel/index.blade.php
+++ resources/views/responsavel/index.blade.php
@@ -10,20 +10,21 @@
             </h1>
             <ol class="breadcrumb">
                 <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-                <li class="active">Pais</li>
+                <li class="active">Responsáveis</li>
             </ol>
         </section>
         <section class="content">
             <div class="row">
                 <div class="col-xs-12">
                     <div class="box">
-                        <div class="box-header"><h3 class="box-title">Listar Pais</h3></div>
+                        <div class="box-header"><h3 class="box-title">Listar Responsáveis</h3></div>
                         <div class="box-body table-responsive">
-                            <table class="table table-hover table-bordered">
+                            <table class="table table-striped table-bordered">
                                 <thead>
                                     <tr>
                                         <th style="width: 10px">#</th>
-                                        <th>Pai</th>
+                                        <th>Responsável</th>
+
                                         <th style="width: 150px">Ação</th>
                                     </tr>
                                 </thead>
@@ -32,6 +33,7 @@
                                         <tr>
                                             <td>{{$registro->id}}</td>
                                             <td>{{ $registro->nome }}</td>
+
                                             <td>
                                                 <a title="Editar" class="btn btn-primary btn-sm" href="{{ route('responsavel.editar',$registro->id) }}"><i class="fa fa-pencil" aria-hidden="true"></i></a>
                                                 <a title="Excluir" class="btn btn-danger btn-sm excluirPai" href="{{ route('responsavel.deletar',$registro->id) }}"><i class="fa fa-trash-o" aria-hidden="true"></i></a>
