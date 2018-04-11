34b6f67b664140c91e5d84ab79342cbd5fed702e
Kaymon Storino <kaymon@edu.univali.br>
2018-04-11 19:29:12 -0300

    adiciona relação com alunos

--- app/Responsavel.php
+++ app/Responsavel.php
@@ -20,6 +20,11 @@ class Responsavel extends Model
         return $this->belongsToMany('App\Aluno', 'aluno_responsavel', 'aluno_id', 'responsavel_id');
     }
 
+    public function alunoss()
+    {
+        return $this->belongsToMany('App\Aluno', 'aluno_responsavel', 'responsavel_id', 'aluno_id');
+    }
+
     public function telefones()
     {
         return $this->belongsTo('App\Telefone', 'telefone_id', 'id');
