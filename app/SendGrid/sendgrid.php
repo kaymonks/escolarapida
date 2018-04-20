da21169b85683f53d859b613feb8916e9eac9ba4
Kaymon Storino <kaymon@edu.univali.br>
2018-04-20 01:07:19 -0300

    altera a chave api da aplicação para o envio de emails

--- app/SendGrid/sendgrid.php
+++ app/SendGrid/sendgrid.php
@@ -10,7 +10,8 @@ $content = new SendGrid\Content("text/html",
     );
 $mail = new SendGrid\Mail($from, $subject, $to, $content);
 
-$apiKey = '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';
+$apiKey = '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';
+//$apiKey = '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';
 $sg = new \SendGrid($apiKey);
 
 $response = $sg->client->mail()->send()->post($mail);
