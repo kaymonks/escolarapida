cb1d42789926ce47b80ed8ef2fef3d4644521bd2
Kaymon Storino <kaymon@edu.univali.br>
2018-04-21 19:12:06 -0300

    modifica header...menu perfil, botão toggle menu apenas no mobile

--- resources/views/layout/_includes/header.blade.php
+++ resources/views/layout/_includes/header.blade.php
@@ -2,67 +2,35 @@
 
     <!-- Main Header -->
     <header class="main-header">
-
-        <!-- Logo -->
-        <a href="index2.html" class="logo">
-            <!-- mini logo for sidebar mini 50x50 pixels -->
-            <span class="logo-mini"><b>ES</b>RAP</span>
-            <!-- logo for regular state and mobile devices -->
-            <span class="logo-lg">Escola<b>Rápida</b></span>
-        </a>
-
         <!-- Header Navbar -->
         <nav class="navbar navbar-static-top" role="navigation">
             <!-- Sidebar toggle button-->
-            <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+            <a href="#" class="visible-xs sidebar-toggle" data-toggle="offcanvas" role="button">
                 <span class="sr-only">Toggle navigation</span>
             </a>
             <!-- Navbar Right Menu -->
             <div class="navbar-custom-menu">
                 <ul class="nav navbar-nav">
                     <!-- Messages: style can be found in dropdown.less-->
-
-
                     <!-- /.messages-menu -->
-
                     <!-- Notifications Menu -->
-
                     <!-- Tasks Menu -->
-
                     <!-- User Account Menu -->
                     <li class="dropdown user user-menu">
                         <!-- Menu Toggle Button -->
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                             <!-- The user image in the navbar-->
-                            <img src="{{ asset("/bower_components/AdminLTE/dist/img/user2-160x160.jpg") }}" class="user-image" alt="User Image">
+                            {{--<img src="{{ asset("/bower_components/AdminLTE/dist/img/user2-160x160.jpg") }}" class="user-image" alt="User Image">--}}
                             <!-- hidden-xs hides the username on small devices so only the image appears. -->
+                            <i class="fa fa-user"></i>
                             <span class="hidden-xs"> @if(Auth::guest())
-
                                 @else
                                     {{ Auth::user()->name }}
                                     @endif </span>
                         </a>
                         <ul class="dropdown-menu">
-                            <!-- The user image in the menu -->
-                            <li class="user-header">
-                                <img src="{{ asset("/bower_components/AdminLTE/dist/img/user2-160x160.jpg") }}" class="img-circle" alt="User Image">
-
-                                <p> @if(Auth::guest())
-
-                                    @else
-                                        {{ Auth::user()->name }}
-                                    @endif
-                                </p>
-                            </li>
-                            <!-- Menu Footer-->
-                            <li class="user-footer">
-                                <div class="pull-left">
-                                    <a href="#" class="btn btn-default btn-flat">Perfil</a>
-                                </div>
-                                <div class="pull-right">
-                                    <a href="{{ route('login.sair') }}" class="btn btn-default btn-flat">Sair</a>
-                                </div>
-                            </li>
+                            {{--<li><a style="padding: 10px 20px;" href="" class="">Perfil</a></li>--}}
+                            <li><a style="padding: 10px 20px;" href="{{ route('login.sair') }}" class="">Sair</a></li>
                         </ul>
                     </li>
                 </ul>
