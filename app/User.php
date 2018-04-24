2667ce56740286a863d3f5bcfb496f279e7bf3ac
Kaymon Storino <kaymon@edu.univali.br>
2018-04-24 19:45:12 -0300

    deleta função de User não utilizada.

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'permission_id',
+        'name', 'login', 'password', 'permission_id',
     ];
 
     /**
@@ -26,14 +26,6 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
-
-    public function ePai()
-    {
-        if ($this->permission_id == 4) {
-            return true;
-        }
-        return false;
-    }
 
     public function getId()
     {
