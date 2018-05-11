09ee8d6905fc2a811e9f5dbbd0ea586b58c08995
Kaymon Storino <kaymon@edu.univali.br>
2018-05-11 00:27:09 -0300

    remove estado cidade de responsavel

--- app/Responsavel.php
+++ app/Responsavel.php
@@ -8,12 +8,7 @@ class Responsavel extends Model
 {
     protected $table = 'responsaveis';
     protected $primaryKey = 'id';
-    protected $fillable = ['nome', 'sexo', 'data_nascimento', 'telefone_id', 'escola_id', 'email', 'endereco', 'cep', 'numero', 'bairro', 'estado', 'cidade', 'user_id'];
-
-//    public function professores()
-//    {
-//        return $this->belongsToMany('App\Professor', 'turma_professores', 'turma_id', 'professor_id');
-//    }
+    protected $fillable = ['nome', 'sexo', 'data_nascimento', 'telefone_id', 'escola_id', 'email', 'endereco', 'cep', 'numero', 'bairro', 'user_id'];
 
     public function alunos()
     {
