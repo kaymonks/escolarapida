8f042d3963f2786edca0c7e8de6b540e7988fc57
Kaymon Storino <kaymon@edu.univali.br>
2018-05-14 17:52:35 -0300

    remove breadcrumb, corrige layout formulário evento

--- resources/views/evento/responsavel/_form.blade.php
+++ resources/views/evento/responsavel/_form.blade.php
@@ -6,39 +6,47 @@
     </div>
 @endif
 
-<div class="form-group">
-    <div class="row">
-        <div class="col-md-12 col-lg-6">
-            <label>Título</label>
-            <input class="form-control" placeholder="Titulo" name="titulo">
+
+<div class="col-md-12">
+    <div class="form-group">
+        <label class="col-sm-4 control-label">Título</label>
+        <div class="col-sm-8 col-md-5">
+            <input class="form-control" placeholder="Titulo" name="titulo" value="{{ old('titulo') }}">
         </div>
-        <div class="col-md-12 col-lg-6">
-            <label>Pais</label>
-            <select class="form-control select2 select2-hidden-accessible" multiple="" name="destinatario[]" data-placeholder="Selecione o(s) pai(s)" style="width: 100%;" tabindex="-1" aria-hidden="true">
+    </div>
+
+    <div class="form-group">
+        <label class="col-sm-4 control-label">Responsável</label>
+        <div class="col-sm-8 col-md-5">
+            <select class="form-control select2 select2-hidden-accessible" multiple="" name="destinatario[]" data-placeholder="Selecione os responsáveis" style="width: 100%;" tabindex="-1" aria-hidden="true">
                 @foreach($responsaveis as $responsavel)
                     <option value="{{ $responsavel->id }}">{{ $responsavel->nome }}</option>
                 @endforeach
             </select>
         </div>
     </div>
-</div>
 
-<div class="form-group">
-    <div class="row">
-        <div class="col-xs-6 col-md-2">
-            <label>Data</label>
-            <div class="form-control-wrapper">
+    <div class="form-group">
+        <label class="col-sm-4 control-label">Data</label>
+
+            <div class="col-sm-8 col-md-5">
                 <input type="text" class="form-control" name="date" id="date">
             </div>
-        </div>
-        <div class="col-xs-6 col-md-2">
-            <label for="">Hora</label>
-            <input type="text" class="form-control" name="time" id="time">
+    </div>
+
+    <div class="form-group">
+        <label class="col-sm-4 control-label">Hora</label>
+
+            <div class="col-sm-8 col-md-5">
+                <input type="text" class="form-control" name="time" id="time">
+            </div>
+
+    </div>
+
+    <div class="form-group">
+        <label class="col-sm-4 control-label">Descrição</label>
+        <div class="col-sm-8 col-md-5">
+            <textarea class="form-control" name="descricao" rows="10"></textarea>
         </div>
     </div>
-</div>
-
-<div class="form-group">
-    <label>Descrição</label>
-    <textarea class="form-control" name="descricao" rows="10"></textarea>
 </div>
--- resources/views/evento/responsavel/enviar.blade.php
+++ resources/views/evento/responsavel/enviar.blade.php
@@ -4,34 +4,22 @@
 
 @section('conteudo')
     <div class="content-wrapper">
-        <section class="content-header">
-            <h1>Pais</h1>
-            <ol class="breadcrumb">
-                <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-                <li><a href="{{ route('eventos') }}"> Eventos</a></li>
-                <li><a href="{{ route('responsaveis') }}"> Pais</a></li>
-                <li class="active">Criar</li>
-            </ol>
-        </section>
         <section class="content">
             <div class="row">
                 <div class="col-xs-12">
                     <div class="box box-primary">
                         <div class="box-header with-border">
-                            <h3 class="box-title">Criar Evento</h3>
+                            <h2 class="box-title">Novo Evento</h2>
                         </div>
                         <!-- /.box-header -->
-                        <form action="{{ route('evento.responsavel.enviar') }}" method="post">
+                        <form class="form-horizontal" action="{{ route('evento.responsavel.enviar') }}" method="post">
                             <div class="box-body">
                                 {{ csrf_field() }}
                                 @include('evento.responsavel._form')
                             </div>
                             <!-- /.box-body -->
                             <div class="box-footer">
-                                <div class="pull-right">
-                                    <a href="{{ route('eventos') }}" class="btn btn-default"><i class="fa fa-times"></i> Cancelar</a>
-                                </div>
-                                <button type="submit" class="btn btn-primary"><i class="fa fa-floppy-o"></i> Salvar</button>
+                                <button type="submit" class="btn btn-primary pull-right"><i class="fa fa-floppy-o"></i> Salvar</button>
                             </div>
                             <!-- /.box-footer -->
                         </form>
