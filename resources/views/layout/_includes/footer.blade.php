158b74b2134d0edae7966f5e9702beb3b1907475
Kaymon Storino <kaymon@edu.univali.br>
2018-05-14 17:55:44 -0300

    altera nome de classe mais genérico para excluir um registro

--- resources/views/layout/_includes/footer.blade.php
+++ resources/views/layout/_includes/footer.blade.php
@@ -1,4 +1,6 @@
-</div>
+
+@include('modules._modules')
+
 <!-- ./wrapper -->
 
 <!-- REQUIRED JS SCRIPTS -->
@@ -12,6 +14,7 @@
 <script src="{{asset("/bower_components/AdminLTE/plugins/datepicker/bootstrap-datepicker.js")}}"></script>
 <!-- Bootstrap 3.3.6 -->
 <script src="{{ asset("/bower_components/AdminLTE/bootstrap/js/bootstrap.min.js") }}"></script>
+<script src="{{ asset("/bower_components/AdminLTE/plugins/bootbox/bootbox.min.js") }}"></script>
 <script type="text/javascript" src="http://momentjs.com/downloads/moment-with-locales.min.js"></script>
 <script type="text/javascript" src="{{asset("/bower_components/AdminLTE/plugins/bootstrap-material-datetimepicker/bootstrap-material-datetimepicker.js")}}"></script>
 <script src="{{asset("/bower_components/AdminLTE/plugins/jQuery-Mask-Plugin/jquery.mask.js")}}"></script>
@@ -22,13 +25,19 @@
 
 <script type="text/javascript">
 
-    $(document).ready(function() {
-        $('#datatables').DataTable({
-            'language': {
-                'url' : '//cdn.datatables.net/plug-ins/1.10.16/i18n/Portuguese-Brasil.json'
-            }
-        });
-    } );
+
+    $('#datatables').DataTable({
+        'language': {
+            'url' : '//cdn.datatables.net/plug-ins/1.10.16/i18n/Portuguese-Brasil.json'
+        }
+    });
+    $('#datatablesOrderByCad').DataTable({
+        'language': {
+            'url' : '//cdn.datatables.net/plug-ins/1.10.16/i18n/Portuguese-Brasil.json'
+        },
+        'order': [[ 0, 'desc' ]]
+    });
+
 
     $('select[name=estado]').change(function () {
         var idEstado = $(this).val();
@@ -72,23 +81,8 @@
         format: 'HH:mm'
     });
 
-    $(".excluirEvento").on("click", function(){
-        return confirm("Você tem certeza que deseja excluir este evento?");
-    });
-    $(".excluirProfessor").on("click", function(){
-        return confirm("Você tem certeza que deseja excluir este Professor?");
-    });
-    $(".excluirPai").on("click", function(){
-        return confirm("Você tem certeza que deseja excluir este Pai?");
-    });
-    $(".excluirAluno").on("click", function(){
-        return confirm("Você tem certeza que deseja excluir este Aluno?");
-    });
-    $(".excluirTurma").on("click", function(){
-        return confirm("Você tem certeza que deseja excluir esta Turma?");
-    });
-    $(".excluirEscola").on("click", function(){
-        return confirm("Você tem certeza que deseja excluir esta Escola?");
+    $(".excluirRegistro").on("click", function(){
+        return confirm("Você tem certeza que deseja excluir este registro?");
     });
 
     $("#inputTelefone").mask('(99) 9999-9999');
